85e7e70aae333c0e4cee001ad94a7ff262652a52
Henry Odunsi <henryodunsi05@gmail.com>
2025-02-11 12:47:48 +0100

    add Tailwind CSS setup, custom button component, and font assets; remove unused CSS

---
+++ src/components/button.jsx
@@ -0,0 +1,10 @@
+
+const button = ({ btnName }) => {
+  return (
+    <div>
+      <button className="btnFont bg-white text-[#0A0C11] w-[169px] text-[16px] rounded-[9px] h-[52px] cursor-pointer " >{btnName}</button>
+    </div>
+  )
+}
+
+export default button
