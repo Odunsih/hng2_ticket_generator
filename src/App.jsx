c785be94eedee5e09ed7e70194bb753de70aa040
Henry Odunsi <henryodunsi05@gmail.com>
2025-02-11 18:47:39 +0100

    implement ticket selection functionality with dynamic styling and state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import Header from "./layout/header"
 import Button from "./components/button.jsx";
+import { useState } from "react";
 
 const App = () => {
+  const [selectedDiv, setSelectedDiv] = useState(null);
+
+  const handleClick = (id) => {
+    setSelectedDiv(id);
+  };
   return (
    <>
    <Header />
    <main className="w-[700px] border-[#0E464F] border-2 p-[48px] rounded-[40px] bg-[#041E23] mt-15 m-auto ">
 
-      <div className="w-[604px] h-[48px] mb-[12px] " >
+      <div className="w-[604px] h-[48px] mb-[25px] " >
         <div className="flex justify-between ">
         <h1 className="btnFont text-[32px]  ">Ticket Selection</h1>
         <p className="text-[16px]/[150%] roboto mt-5 font-light ">Step 1/3</p>
@@ -30,40 +36,54 @@ const App = () => {
       <div className="w-[556px] h-[142px] p-[16px] mt-[16px] mb-[16px] rounded-[24px] bg-[#052228] border-[#07373F] border-[1px] flex justify-evenly">
 
         {/* FREE */}
-        <div className="flex flex-col justify-between w-[158px] h-[110px] p-[12px] rounded-[12px]  cursor-pointer border-[#197686] border-[1px] transition hover:bg-[#12464E]">
-        <div className="w-[80px] h-[38px] ">
-            <h3 className="roboto font-semibold  text-[20px]/[110%] ">Free</h3>
-          </div>
-          <div className="w-[138px] h-[49px] mt-[12px]">
-            <h3 className="roboto text-[#FAFAFA] text-[16px]/[150%] mb-[4px]">REGULAR ACCESS</h3>
-            <p className="roboto text-[14px]/[150%]">20/52</p>
-          </div>
-          
+        <div
+        className={`flex flex-col justify-between w-[158px] h-[110px] p-[12px] rounded-[12px] cursor-pointer border-[#197686] border-[1px] transition hover:bg-[#2C545B] ${
+          selectedDiv === "free" ? "bg-[#12464E]" : ""
+        }`}
+        onClick={() => handleClick("free")}
+      >
+        <div className="w-[80px] h-[38px]">
+          <h3 className="roboto font-semibold text-[20px]/[110%]">Free</h3>
         </div>
+        <div className="w-[138px] h-[49px] mt-[12px]">
+          <h3 className="roboto text-[#FAFAFA] text-[16px]/[150%] mb-[4px]">
+            REGULAR ACCESS
+          </h3>
+          <p className="roboto text-[14px]/[150%]">20/52</p>
+        </div>
+      </div>
 
-        {/* $50 */}
-        <div className="flex flex-col justify-between w-[158px] h-[110px] p-[12px] rounded-[12px] cursor-pointer border-[#197686] border-[1px] transition hover:bg-[#12464E]">
-        <div className="w-[80px] h-[38px] ">
-            <h3 className="roboto font-semibold text-[20px]/[110%] ">$50</h3>
-          </div>
-          <div className="w-[138px] h-[49px] mt-[12px]">
-            <h3 className="roboto text-[16px]/[150%] mb-[4px]">VIP ACCESS</h3>
-            <p className="roboto text-[14px]/[150%]">20/52</p>
-          </div>
-          
+      {/* $50 */}
+      <div
+        className={`flex flex-col justify-between w-[158px] h-[110px] p-[12px] rounded-[12px] cursor-pointer border-[#197686] border-[1px] transition hover:bg-[#2C545B] ${
+          selectedDiv === "50" ? "bg-[#12464E]" : ""
+        }`}
+        onClick={() => handleClick("50")}
+      >
+        <div className="w-[80px] h-[38px]">
+          <h3 className="roboto font-semibold text-[20px]/[110%]">$50</h3>
         </div>
+        <div className="w-[138px] h-[49px] mt-[12px]">
+          <h3 className="roboto text-[16px]/[150%] mb-[4px]">VIP ACCESS</h3>
+          <p className="roboto text-[14px]/[150%]">20/52</p>
+        </div>
+      </div>
 
-        {/* $150 */}
-        <div className="flex flex-col justify-between w-[158px] h-[110px] p-[12px] rounded-[12px] cursor-pointer border-[#197686] border-[1px] transition hover:bg-[#12464E]">
-        <div className="w-[80px] h-[38px] ">
-            <h3 className="roboto font-semibold text-[20px]/[110%] ">$150</h3>
-          </div>
-          <div className="w-[138px] h-[49px] mt-[12px] ">
-            <h3 className="roboto text-[16px]/[150%] mb-[4px]">VVIP ACCESS</h3>
-            <p className="roboto text-[14px]/[150%]">20/52</p>
-          </div>
-          
+      {/* $150 */}
+      <div
+        className={`flex flex-col justify-between w-[158px] h-[110px] p-[12px] rounded-[12px] cursor-pointer border-[#197686] border-[1px] transition hover:bg-[#2C545B] ${
+          selectedDiv === "150" ? "bg-[#12464E]" : ""
+        }`}
+        onClick={() => handleClick("150")}
+      >
+        <div className="w-[80px] h-[38px]">
+          <h3 className="roboto font-semibold text-[20px]/[110%]">$150</h3>
         </div>
+        <div className="w-[138px] h-[49px] mt-[12px]">
+          <h3 className="roboto text-[16px]/[150%] mb-[4px]">VVIP ACCESS</h3>
+          <p className="roboto text-[14px]/[150%]">20/52</p>
+        </div>
+      </div>
       </div>
       </div>
      
