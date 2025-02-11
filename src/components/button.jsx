2ab85abc2bd4a509adeaee576dd90edc996ddc1a
Henry Odunsi <henryodunsi05@gmail.com>
2025-02-11 20:52:30 +0100

    refactor button component styles for improved consistency and responsiveness

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,7 +1,7 @@
 
 const button = ({ btnName, arr, onClick, variant = "primary", size = "md" }) => {
     const baseStyles =
-    " pr-[16px] pl-[16px] lg:pr-[24px] lg:pl-[24px] text-[#0A0C11] rounded-[8px] cursor-pointer";
+    "btnFont pr-[16px] pl-[16px] lg:pr-[24px] lg:pl-[24px] text-[#0A0C11] rounded-[8px] cursor-pointer";
   
   const sizeStyles = {
     sm: "px-3 py-1 text-sm",
@@ -10,8 +10,8 @@ const button = ({ btnName, arr, onClick, variant = "primary", size = "md" }) => 
   };
 
   const variantStyles = {
-    ticket: "btnFont bg-white w-[140.6px] h-[44px] text-[14px] lg:w-[170px] lg:h-[52px] lg:text-[16px] transition hover:bg-[#24A0B5] hover:border-[#D9D9D9] hover:text-[#D9D9D9] hover:border-[.1px]",
-    select: "text-[#24A0B5] border-[#24A0B5] border-1 w-[266px] h-[48px] pr-[24px] pl-[12px] transition hover:bg-[#24A0B5] hover:text-[#fff]",
+    ticket: " bg-white w-[140.6px] h-[44px] text-[14px] lg:w-[170px] lg:h-[52px] lg:text-[16px] transition hover:bg-[#24A0B5] hover:border-[#D9D9D9] hover:text-[#D9D9D9] hover:border-[.1px]",
+    select: "text-[#24A0B5] text-[16px] border-[#24A0B5] border-1 w-[266px] h-[48px] pr-[24px] pl-[12px] transition hover:bg-[#24A0B5] hover:text-[#fff]",
     outline: "text-[#24A0B5] border-[#24A0B5] border-1 w-[214px] h-[48px] pr-[24px] pl-[12px] transition hover:bg-[#24A0B5] hover:text-[#fff]",
     danger: "bg-red-600 text-white hover:bg-red-700",
   };
