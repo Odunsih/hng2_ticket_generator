155d54286ccfff2dae5239d211144bb1b46108e5
Henry Odunsi <henryodunsi05@gmail.com>
2025-02-12 18:32:18 +0100

    refactor TicketConfirmation component for improved layout and styling; adjust CSS for better alignment and responsiveness

--- src/components/steps/TicketConfirmation.jsx
+++ src/components/steps/TicketConfirmation.jsx
@@ -25,11 +25,12 @@ const TicketConfirmation = ({
                   <h1 className="text-[32px]/[auto] text-center alatsi mb-[16px] ">Your Ticket is Booked!</h1>
                   <p className="roboto text-center text-[16px]/[150%]  ">Check your email for a copy or you can <strong>download</strong></p>
                 </div>
-                <div className="w-[100%] h-[664px] mt-[50px] sm:mt-0 pt-[32px] pb-[32px] pl-[21px] pr-[21px]">
-                  <div className=" back w-[300px] h-[600px] bg-[#12464E] border-[#24A0B5] border-[1px] m-auto ">
+                <div className="w-full max-w-[604px] min-h-[664px] flex justify-center items-center py-8">
+                    {/* TICKET */}
+                  <div className=" back w-[300px] h-[600px] bg-[#12464E] border-[#24A0B5] border-[1px] relative">
                     {/* EVENT DETAILS */}
-                    <div className="w-[260px] h-[446px] p-[14px] mt-[20px] relative left-[20px] rounded-[16px] border-[#24A0B5] border-[1px] ">
-                      <div className="w-[232px] h-[416px]">
+                    <div className=" w-[90%] max-w-[260px] h-[446px] p-[14px] m-auto mt-[20px] absolute left-[20px] rounded-[16px] border-[#24A0B5] border-[1px] ">
+                      <div className="max-w-[232px] h-[416px] m-auto">
                         <div className="w-[100%] lg:w-[175px] h-[76px] m-auto text-center">
                           <h1 className="road text-[34px]/[100%]">Techember Fest &quot;25</h1>
                           <p className="roboto text-[10px]/[150%]">📍 04 Rumens road, Ikoyi, Lagos </p>
@@ -38,7 +39,7 @@ const TicketConfirmation = ({
                         <div className="rounded-[12px] w-[140px] h-[140px] m-auto border-[#249fb580] border-[5px] ">
                           <img className="w-[100%] h-[100%]" src={formData.profilePhoto || User} alt="user image" />
                         </div>
-                        <div className="w-[232px] h-[160px] p-[4px] bg-[#08343C] border-[#133D44] border-[1px] rounded-[24px] mt-[20px] ">
+                        <div className="w-[95%] max-w-[232px] h-[160px] p-[4px] bg-[#08343C] border-[#133D44] border-[1px] rounded-[24px] mt-[20px] ">
                           <div className="w-[224px] h-[45px] flex pbd ">
                             <div className="w-[108px] h-[45px] p-[4px] bdn ">
                               <p className="roboto text-[10px]/[150%] opacity-[0.33]">Name</p>
@@ -66,15 +67,15 @@ const TicketConfirmation = ({
                         </div>
                       </div>
                     </div>
-                    <div className="bod w-[30px] h-[30px] bg-[#041E23] rounded-[50%] relative right-[15px] bottom-[470px] rotate-[30deg] "></div>
-                    <div className="bod w-[30px] h-[30px] bg-[#041E23] rounded-[50%] relative left-[280px] bottom-[501px] rotate-[140deg] "></div>
-                    <div className="bod w-[30px] h-[30px] bg-[#041E23] rounded-[50%] relative left-[285px] bottom-[60px] rotate-[180deg] "></div>
-                    <div className="bod w-[30px] h-[30px] bg-[#041E23] rounded-[50%] relative right-[15px] bottom-[90px]  "></div>
-                    <div className="bod w-[30px] h-[30px] bg-[#041E23] rounded-[50%] relative right-[10px] bottom-[12px] rotate-[-40deg] "></div>
-                    <div className="bod w-[30px] h-[30px] bg-[#041E23] rounded-[50%] relative left-[280px] bottom-[37px] rotate-[-140deg] "></div>
-                    <hr className="zig relative bottom-[167px]" />
-                    <div className="w-[236px] h-[68px] relative bottom-[150px] left-[30px] ">
-                      <img src={BarCode} alt="" />
+                    <div className="bod w-[30px] h-[30px] bg-[#08252B] lg:bg-[#041E23] rounded-[50%] absolute -left-[15px] bottom-[574px] rotate-[30deg] "></div>
+                    <div className="bod w-[30px] h-[30px] bg-[#08252B] lg:bg-[#041E23] rounded-[50%] absolute -right-[12px] bottom-[580px] rotate-[140deg] "></div>
+                    <div className="bod w-[30px] h-[30px] bg-[#08252B] lg:bg-[#041E23] rounded-[50%] absolute -right-[15px] bottom-[90px] rotate-[180deg] "></div>
+                    <div className="bod w-[30px] h-[30px] bg-[#08252B] lg:bg-[#041E23] rounded-[50%] absolute -left-[15px] bottom-[90px]  "></div>
+                    <div className="bod w-[30px] h-[30px] bg-[#08252B] lg:bg-[#041E23] rounded-[50%] absolute -left-[10px] top-[580px] rotate-[-40deg] "></div>
+                    <div className="bod w-[30px] h-[30px] bg-[#08252B] lg:bg-[#041E23] rounded-[50%] absolute -right-[15px] top-[580px] rotate-[-140deg] "></div>
+                    <hr className="zig absolute bottom-[105px] w-full" />
+                    <div className="max-w-[236px] m-auto h-[68px] absolute bottom-[15px] left-[30px] ">
+                      <img src={BarCode} alt="Ticket barcode" className="w-full h-full" />
                     </div>
                   </div>
                 </div>
